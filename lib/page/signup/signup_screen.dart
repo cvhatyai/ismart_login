14edf1ff33c1334436398da49a9366f596e87183
harzancs <arzan.zan9@gmail.com>
2021-03-31 11:58:08 +0700

    Initial commit

---
+++ lib/page/signup/signup_screen.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ismart_login/style/page_style.dart';
+import 'package:ismart_login/style/font_style.dart';
+
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  Widget formlogin() {
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            style: TextStyle(fontFamily: FontStyles().FontFamily, fontSize: 24),
+            decoration: InputDecoration(
+              hintText: 'เบอร์โทรศัพท์',
+              hintStyle:
+                  TextStyle(fontFamily: FontStyles().FontFamily, fontSize: 24),
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(0), // add padding to adjust icon
+                child: Icon(
+                  Icons.phone_iphone,
+                  size: 26,
+                ),
+              ),
+            ),
+          ),
+          TextFormField(
+            style: TextStyle(fontFamily: FontStyles().FontFamily, fontSize: 24),
+            decoration: InputDecoration(
+              hintText: 'รหัสผ่าน',
+              hintStyle:
+                  TextStyle(fontFamily: FontStyles().FontFamily, fontSize: 24),
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(0), // add padding to adjust icon
+                child: Icon(
+                  Icons.lock,
+                  size: 26,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+          ),
+          GestureDetector(
+            onTap: () {
+              print('เข้าสู่ระบบ');
+            },
+            child: Container(
+              padding: EdgeInsets.only(left: 25, right: 25),
+              decoration: BoxDecoration(
+                color: Color(0xFF079CFD),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Text(
+                'เข้าสู่ระบบ',
+                style: TextStyle(
+                    fontFamily: FontStyles().FontFamily,
+                    color: Colors.white,
+                    fontSize: 36),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.height,
+        decoration: StylePage().background,
+        child: SafeArea(
+          child: Container(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'iSmartLogin',
+                    style: TextStyle(
+                        fontFamily: FontStyles().FontFamily,
+                        fontSize: 46,
+                        color: Colors.white,
+                        fontWeight: FontWeight.normal),
+                  ),
+                ),
+                Container(
+                  padding:
+                      EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 20),
+                  width: MediaQuery.of(context).size.width,
+                  decoration: StylePage().boxWhite,
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 15),
+                        alignment: Alignment.center,
+                        width: 100,
+                        height: 100,
+                        decoration: new BoxDecoration(
+                          color: Color(0xFF18C0FF),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.white,
+                          size: 75,
+                        ),
+                      ),
+                      Text(
+                        'เข้าใช้งาน',
+                        style: TextStyle(
+                            fontFamily: FontStyles().FontFamily, fontSize: 46),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 40, left: 20, right: 20),
+                        child: formlogin(),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            GestureDetector(
+                              child: Text('ลืมรหัสผ่าน',
+                                  style: TextStyle(
+                                      fontFamily: FontStyles().FontFamily,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24)),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 10, right: 10),
+                              child: Text(
+                                '|',
+                                style: TextStyle(fontSize: 30),
+                              ),
+                            ),
+                            GestureDetector(
+                              child: Text('ลงทะเบียน',
+                                  style: TextStyle(
+                                      fontFamily: FontStyles().FontFamily,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24)),
+                            )
+                          ],
+                        ),
+                      ),
+                      Divider(),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            MaterialButton(
+                              onPressed: () {
+                                print('facebook');
+                              },
+                              color: Color(0xFF3B5998),
+                              child: FaIcon(
+                                FontAwesomeIcons.facebookF,
+                                color: Colors.white,
+                                size: 26,
+                              ),
+                              padding: EdgeInsets.all(12),
+                              shape: CircleBorder(),
+                            ),
+                            MaterialButton(
+                              onPressed: () {
+                                print('line');
+                              },
+                              color: Color(0xFF00C300),
+                              child: Image.asset(
+                                'assets/images/social/line.png',
+                                width: 26,
+                                fit: BoxFit.fill,
+                              ),
+                              padding: EdgeInsets.all(12),
+                              shape: CircleBorder(),
+                            ),
+                            MaterialButton(
+                              onPressed: () {
+                                print('apple');
+                              },
+                              color: Color(0xFFA3AAAE),
+                              child: FaIcon(
+                                FontAwesomeIcons.apple,
+                                color: Colors.white,
+                                size: 26,
+                              ),
+                              padding: EdgeInsets.all(12),
+                              shape: CircleBorder(),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/style/font_style.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class FontStyles {
+  String FontFamily = "thaisanslite_r1";
+  double FontSizeTitle = 19.0;
+  double FontSizeData = 16.0;
+  double FontSizeSubData = 14.0;
+  double FontSizeLabel = 16.0;
+  FontStyles() : super();
+}
