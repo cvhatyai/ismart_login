50161c3411917ffcbec0c15017ed5eec0274efda
harzancs <arzan.zan9@gmail.com>
2021-04-20 15:48:33 +0700

    zan 20-4-64 1548

--- lib/page/front/model/attendHistoryResult.dart
+++ lib/page/front/model/attendHistoryResult.dart
@@ -2,11 +2,19 @@ class ItemsAttendHistoryResult {
   final String TIME;
   final String TIME_STATUS;
   final String STATUS;
+  final String IMAGES;
+  final String LAT;
+  final String LNG;
+  final String NOTE;
 
   ItemsAttendHistoryResult({
     this.TIME,
     this.TIME_STATUS,
     this.STATUS,
+    this.IMAGES,
+    this.LAT,
+    this.LNG,
+    this.NOTE,
   });
 
   factory ItemsAttendHistoryResult.fromJson(Map<String, dynamic> json) {
@@ -14,6 +22,10 @@ class ItemsAttendHistoryResult {
       TIME: json['time'],
       TIME_STATUS: json['time_status'],
       STATUS: json['status'],
+      IMAGES: json['images'],
+      LAT: json['lat'],
+      LNG: json['lng'],
+      NOTE: json['note'],
     );
   }
 }
